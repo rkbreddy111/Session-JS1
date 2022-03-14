f021a834332acbee3c3036ee82084e0233657bfc
rkbreddy111 <99252517+rkbreddy111@users.noreply.github.com>
2022-03-14 19:10:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+//my first program
+console.log("Hello World")
+
+// Data Types
+// String = Alphabets
+// INT - Whole Number - example 1,10,15
+// Float - Decimal and Whole number - examples 10,15,1.5,2.4...
+// Boolean - True or False
+
+// Example of few Data Types
+// String - 
+console.log("Bhaskar Reddy")
+
+// INT
+console.log(39)
+    // Float 
+console.log(3.5)
+
+// Boolean - all the letters will be small case
+console.log(true)
+
+// Examples for a Variable
+
+console.log("Bhaskar is studying in a college")
+console.log("He is 20 years old")
+console.log("This college he studies in is Raja college")
+
+// Start of Variable
+
+var nam = "Reddy"
+var age = 25
+var college = "XYZ college"
+
+console.log(nam + " is studying in a college")
+console.log(" His age is " + age)
+console.log(" He is studying in " + college)
+
+// var = these are storage units. can be change or modified any number of times.
+var q = 15
+console.log(q)
+
+// Let = once declared, can not be changed after that
+
+let a = 35
+console.log(a)
+
+// can not be changed
+// let a = 45
+// console.log(a)
+
+let b
+b = " mersk training "
+console.log(b)
+
+// CONST - used for constants. once declared, can not be changed. you will have to declare and assign the value in the same line
+
+var x = 20
+var y = 15
+
+const z = x + y
+console.log(z)
+
+var x = 20
+var y = 15
+
+const m = x - y
+console.log(m)
+
+var x = 20
+var y = 15
+
+const p = x * y
+console.log(p)
+
+var x = 20
+var y = 15
+
+const n = x / y
+console.log(n)
+
+
+// This will not work
+// const t
+// t = 100
+// console.log(t)
